6cd0f212fa2249a4a2adb57ddbb92cf7875e855e
Adorilson Bezerra <adorilson.bezerra@ifrn.edu.br>
2019-07-12 21:55:05 -0300

    Criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=39):
+    def __init__(self, *filhos, nome = None, idade=39):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
 
     def cumprimentar(self):
         return f'Olá {id(self)} '
 
 if __name__=='__main__':
-    p = Pessoa('Aldiene')
+    alice = Pessoa(nome='Alice')
+    alexia = Pessoa(nome='Alexia')
+
+    p = Pessoa(alice, alexia, nome='Adorilson')
+
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
@@ -16,3 +21,6 @@ if __name__=='__main__':
     p.nome = 'Adorilson'
     print(p.nome)
     print(p.idade)
+
+    for filho in p.filhos:
+        print(filho.nome)
